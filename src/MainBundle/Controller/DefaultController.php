f0ca133ab29a996fed26dc40a24911a8afaaeef0
Ouni-Chaker <elouni80@yahoo.fr>
2017-07-04 11:51:45 +0100

    les formulaires sans entite

--- src/MainBundle/Controller/DefaultController.php
+++ src/MainBundle/Controller/DefaultController.php
@@ -3,11 +3,37 @@
 namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('MainBundle:Default:index.html.twig');
+    }
+    
+    public function testAction(Request $request)
+    {
+        $name = $email = $objet = null;
+        $form = $this->createFormBuilder()
+                ->add('name')
+                ->add('email')
+                ->add('objet',  ChoiceType::class,array( 'choices'=>array(''=>null,'Yes'=>'Yes','No'=>'No')))
+                ->add('send',  SubmitType::class,array('label'=>'ok'))
+                ->getForm();
+        
+        $form->handleRequest($request);
+        if($form->isValid()){
+        $name = $form["name"]->getData();
+        $email = $form["email"]->getData();
+        $objet = $form["objet"]->getData();
+        }
+        
+        return $this->render('MainBundle:Default:test.html.twig',array('form'=> $form->createView(),
+                                                                        'name'=>$name,
+                                                                        'email'=>$email,
+                                                                        'objet'=>$objet));
     }
 }
